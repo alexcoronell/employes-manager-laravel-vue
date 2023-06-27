7b32eb5173d012f1654326dfff10f95a16fce934
Alex Coronell <alexander@catapultsteel.com>
2023-06-27 15:55:34 -0500

    Create department partial completed, error in message error

--- resources/js/Pages/Departments/Create.vue
+++ resources/js/Pages/Departments/Create.vue
@@ -1,10 +1,20 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+const form = useForm({
+    name: ''
+})
+
 </script>
 
+
 <template>
-    <Head title="Create Departments" />
+    <Head title="Create Department" />
 
     <AuthenticatedLayout>
         <template #header>
@@ -13,8 +23,13 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-<!--                     <div class="p-6 text-gray-900">You're logged in!</div> -->
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+                    <form @submit.prevent="$event => form.post(route('departments.store'))" class="mt-6 space-y-6 max-w-xl">
+                        <InputLabel for="name" value="Department"></InputLabel>
+                        <TextInput type="text" id="name" v-model="form.name" autofocus required class="mt-1 block w-full"></TextInput>
+                        <!-- <InputError :message="form.errors.name" class="mt-2"></InputError> -->
+                        <PrimaryButton :disable="form.processing"><i class="fa-solid fa-save"></i> Save</PrimaryButton>
+                    </form>
                 </div>
             </div>
         </div>
