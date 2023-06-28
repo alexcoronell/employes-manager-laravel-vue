9c84c822ac70db51902aef9a93b6b84c0f7265ff
Alex Coronell <alexander@catapultsteel.com>
2023-06-28 10:39:21 -0500

    Graphic page loaded, employees data doesn't show on graphic

--- resources/js/Pages/Employees/Graphic.vue
+++ resources/js/Pages/Employees/Graphic.vue
@@ -1,6 +1,43 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartsJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ArcElement } from 'chart.js';
+ChartsJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ArcElement);
+
+const props = defineProps({
+    data: { type: Object }
+})
+
+const departments = ref([]);
+const employees = ref([]);
+const chartData = ref([]);
+const chartOptions = ref([]);
+const colors = ref([]);
+
+const random = () => {
+    return Math.floor(Math.random() * 256);
+};
+
+props.data.map( (row) => {
+    departments.value.push(row.name),
+    employees.value.push(row.counr),
+    colors.value.push("rgb("+random()+","+random()+","+random()+")")
+});
+
+chartOptions.value = { resposive: true };
+chartData.value = {
+    labels: departments.value,
+    datasets: [
+        { 
+            label: 'Employees',
+            data: employees.value,
+            backgroundColor: colors 
+        }
+    ]
+}
+
 </script>
 
 <template>
@@ -12,9 +49,9 @@ import { Head } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-<!--                     <div class="p-6 text-gray-900">You're logged in!</div> -->
+                    <Bar :data="chartData" :options="chartOptions"></Bar>
                 </div>
             </div>
         </div>
